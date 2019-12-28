e84f07a790604106772cecd3a1b940a0e328324b
Sarthak Chadha <Sarthak_Chadha@external.mckinsey.com>
2019-12-28 11:58:41 +0530

    Second Commit - added steps of git

--- src/Git.java
+++ src/Git.java
@@ -5,5 +5,23 @@ public class Git {
 //
 //	1. Github account > Sign Up > Repo (for multiple projectS)
 //	2. Gitbash install on your system
+	
+	
+	//Added new things
+//	Create new Repository
+//	Save REpo url - https://github.com/Sarthak-1234/GitSession1.git
+//		git init
+//		git add . (for alling all the things - for particular file git add file_name)
+//		git commit -m "first commit"
+//		git remote add origin https://github.com/Sarthak-1234/GitSession1.git
+//		git push origin master
+//		
+//		
+//		
+//	
+//		
+//		eclipse > .git folder (hidden) - local repo > online github repo
 
+	
+	//add > commit > push
 }
